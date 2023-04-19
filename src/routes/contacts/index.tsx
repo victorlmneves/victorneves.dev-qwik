57c55deba33b9b2e595d088a5df631cc4163777a
Victor Neves <info@victorneves.dev>
2023-04-19 13:37:02 +0100

    Extend head tags

--- src/routes/contacts/index.tsx
+++ src/routes/contacts/index.tsx
@@ -14,11 +14,55 @@ export default component$(() => {
 })
 
 export const head: DocumentHead = {
-  title: 'Contacts page',
+  title: 'Contact Victor Neves - Senior creative developer',
   meta: [
     {
       name: 'description',
-      content: 'Copy of my personal website orinally made with Vue, using Qwik',
+      content:
+        'Victor Neves - Senior creative developer | Copy of my personal website orinally made with Vue, using Qwik',
+    },
+    {
+      property: 'og:site_name',
+      content:
+        'Victor Neves - Senior creative developer | Copy of my personal website orinally made with Vue, using Qwik',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:url',
+      content: 'https://www.victorneves.dev',
+    },
+    {
+      property: 'og:title',
+      content:
+        'Victor Neves - Senior creative developer | Copy of my personal website orinally made with Vue, using Qwik',
+    },
+    {
+      property: 'og:description',
+      content: 'Senior creative developer based in Portugal',
+    },
+    {
+      property: 'og:image',
+      content: 'https://res.cloudinary.com/vitorneves/image/upload/v1617370600/victorneves-static-images/me.jpg',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:title',
+      content:
+        'Victor Neves - Senior creative developer | Copy of my personal website orinally made with Vue, using Qwik',
+    },
+    {
+      name: 'twitter:description',
+      content: 'Senior creative developer based in Portugal',
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://res.cloudinary.com/vitorneves/image/upload/v1617370600/victorneves-static-images/me.jpg',
     },
   ],
 }

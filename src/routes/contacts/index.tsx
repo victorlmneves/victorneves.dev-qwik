b347f215957dec7fe43124191a0b5289e0388aee
Victor Neves <info@victorneves.dev>
2023-04-19 17:04:31 +0100

    Delete unused import

--- src/routes/contacts/index.tsx
+++ src/routes/contacts/index.tsx
@@ -2,7 +2,6 @@ import { component$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import ContactAddress from '~/components/visual/contacts/contacts-address'
 import ContactForm from '~/components/visual/contacts/contacts-form'
-import { headTags } from '~/utils/head-tags'
 import './index.scss'
 
 export default component$(() => {
